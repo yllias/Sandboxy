5fdb1593010d50440a74e2f14b64e087aeaf04f0
Yll <yll.kryeziu@tum.de>
2024-12-23 14:56:27 +0100

    Initial commit

---
+++ Sandboxy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sandboxy
+//
+//  Created by Yll Kryeziu on 23.12.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sandboxy/SandboxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxyApp.swift
+//  Sandboxy
+//
+//  Created by Yll Kryeziu on 23.12.24.
+//
+
+import SwiftUI
+
+@main
+struct SandboxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
